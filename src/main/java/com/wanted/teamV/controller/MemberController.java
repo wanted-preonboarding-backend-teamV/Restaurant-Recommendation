765f2521f896275d20dd88f77fbdb233401464c1
shinmin <shinmin9812@naver.com>
2023-11-01 18:18:19 +0900

    feat#3 - 사용자 API 구현 - MemberRepository, MemberService 인터페이스 구현 - MemberServiceImpl 회원가입, 로그인, 설정 업데이트, 조회 기능 구현, Account가 중복됐는지 확인하기 위한 validateUniqueAccount 구현 - MemberController 회원가입, 로그인, 설정 업데이트, 조회 API 구현

--- src/main/java/com/wanted/teamV/controller/MemberController.java
+++ src/main/java/com/wanted/teamV/controller/MemberController.java
@@ -1,9 +1,76 @@
 package com.wanted.teamV.controller;
 
+import com.wanted.teamV.dto.LoginMember;
+import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.dto.req.MemberLoginReqDto;
+import com.wanted.teamV.dto.req.MemberUpdateReqDto;
+import com.wanted.teamV.dto.res.MemberInfoResDto;
+import com.wanted.teamV.dto.res.MemberTokenResDto;
+import com.wanted.teamV.entity.Member;
+import com.wanted.teamV.exception.CustomException;
+import com.wanted.teamV.exception.ErrorCode;
+import com.wanted.teamV.repository.MemberRepository;
+import com.wanted.teamV.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @PostMapping()
+    public ResponseEntity<String> join(
+            @Valid @RequestBody MemberJoinReqDto memberJoinReqDto
+    ) {
+        memberService.join(memberJoinReqDto);
+        return ResponseEntity.ok("OK");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MemberTokenResDto> login(
+            @RequestBody MemberLoginReqDto memberLoginReqDto
+    ) {
+        MemberTokenResDto memberTokenResDto = memberService.login(memberLoginReqDto);
+        return ResponseEntity.ok(memberTokenResDto);
+    }
+
+    @GetMapping()
+    public ResponseEntity<MemberInfoResDto> getMember(
+            @AuthenticationPrincipal LoginMember loginMember
+    ) {
+        MemberInfoResDto response = memberService.getMember(loginMember.id());
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<MemberInfoResDto> updateMember(
+            @AuthenticationPrincipal LoginMember loginMember,
+            @RequestBody MemberUpdateReqDto memberUpdateReqDto
+    ) {
+        Member member = memberRepository.findById(loginMember.id()).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        Double lat = memberUpdateReqDto.lat();
+        Double lon = memberUpdateReqDto.lon();
+        Boolean recommend = memberUpdateReqDto.recommend();
+
+        if (memberUpdateReqDto.lat() == null) {
+            lat = member.getLat();
+        }
+
+        if (memberUpdateReqDto.lon() == null) {
+            lon = member.getLon();
+        }
+
+        if (memberUpdateReqDto.recommend() == null) {
+            recommend = member.getRecommend();
+        }
+
+        MemberInfoResDto response = memberService.updateMember(loginMember.id(), lat, lon, recommend);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/wanted/teamV/repository/MemberRepository.java
+++ src/main/java/com/wanted/teamV/repository/MemberRepository.java
@@ -1,7 +1,24 @@
 package com.wanted.teamV.repository;
 
 import com.wanted.teamV.entity.Member;
+import com.wanted.teamV.exception.CustomException;
+import com.wanted.teamV.exception.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByAccount(String account);
+
+    Optional<Member> findByAccount(String account);
+
+    default Member getByAccount(String account) {
+        return findByAccount(account).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+    }
+
+    @Modifying
+    @Query("UPDATE Member m SET m.lat = :lat, m.lon = :lon, m.recommend = :recommend WHERE m.id = :memberId")
+    void updateMemberFields(Long memberId, Double lat, Double lon, Boolean recommend);
 }
