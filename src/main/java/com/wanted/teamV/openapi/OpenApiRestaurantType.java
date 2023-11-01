799799fbfa16431c06636718eaf88e62356e8f1e
jjungyeun <wjyddd@naver.com>
2023-11-01 17:19:46 +0900

    feat #2 - 데이터 수집 및 전처리 로직 구현
    - API를 호출해서 데이터를 수집하는 메소드 구현
    - raw 데이터 필터링 및 전처리 메소드 구현
    - raw 데이터를 엔티티로 변환하는 메소드 구현

--- src/main/java/com/wanted/teamV/openapi/OpenApiRestaurantType.java
+++ src/main/java/com/wanted/teamV/openapi/OpenApiRestaurantType.java
@@ -1,19 +1,25 @@
 package com.wanted.teamV.openapi;
 
 public enum OpenApiRestaurantType {
-    LUNCH("Genrestrtlunch"),
-    CHINA_FOOD("Genrestrtchifood"),
-    JAPAN_FOOD("Genrestrtjpnfood"),
-    FAST_FOOD("Genrestrtfastfood")
+    LUNCH("Genrestrtlunch", "김밥(도시락)"),
+    CHINA_FOOD("Genrestrtchifood", "중국식"),
+    JAPAN_FOOD("Genrestrtjpnfood", "일식"),
+    FAST_FOOD("Genrestrtfastfood", "패스트푸드")
     ;
 
-    private String path;
+    private final String path;
+    private final String type;
 
-    OpenApiRestaurantType(String path) {
+    OpenApiRestaurantType(String path, String type) {
         this.path = path;
+        this.type = type;
     }
 
     public String getPath() {
         return path;
+    }
+
+    public String getType() {
+        return type;
     }
 }
