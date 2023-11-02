d727e789f7bd9e014b2895a1fa2509a1c7a751bc
shinmin <shinmin9812@naver.com>
2023-11-02 14:26:09 +0900

    test #3 - 사용자 로그인 API 테스트 코드 구현 - Member 엔티티에 테스트용 객체 생성 메소드 구현 - 사용자 로그인 성공, 실패 테스트 코드 구현

--- src/test/java/com/wanted/teamV/controller/MemberControllerTest.java
+++ src/test/java/com/wanted/teamV/controller/MemberControllerTest.java
@@ -2,6 +2,8 @@ package com.wanted.teamV.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.dto.req.MemberLoginReqDto;
+import com.wanted.teamV.dto.res.MemberTokenResDto;
 import com.wanted.teamV.entity.Member;
 import com.wanted.teamV.exception.CustomException;
 import com.wanted.teamV.exception.ErrorCode;
@@ -15,7 +17,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -75,5 +80,40 @@ class MemberControllerTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 - 성공")
+    public void login() throws Exception {
+        //given
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "test1234!@#$");
+        MemberTokenResDto tokenResponse = new MemberTokenResDto("123456789qwerttyadsccvzzsadwdqadwa.asddwdad");
+
+        when(memberService.login(request)).thenReturn(tokenResponse);
+
+        //when & then
+        mockMvc.perform(post("/members/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("로그인 - 실패(잘못된 비밀번호)")
+    public void login_invalid_password() throws Exception {
+        //given
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "wrongPassword");
+
+        when(memberService.login(request)).thenThrow(new CustomException(ErrorCode.INVALID_PASSWORD));
+
+        //when & then
+        mockMvc.perform(post("/members/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+
     }
 }
--- src/test/java/com/wanted/teamV/service/MemberServiceImplTest.java
+++ src/test/java/com/wanted/teamV/service/MemberServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.wanted.teamV.service;
 
 import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.dto.req.MemberLoginReqDto;
+import com.wanted.teamV.dto.res.MemberTokenResDto;
 import com.wanted.teamV.entity.Member;
 import com.wanted.teamV.exception.CustomException;
 import com.wanted.teamV.exception.ErrorCode;
 import com.wanted.teamV.repository.MemberRepository;
+import com.wanted.teamV.service.impl.AuthTokenCreator;
 import com.wanted.teamV.service.impl.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,8 +21,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -34,6 +40,9 @@ class MemberServiceImplTest {
 
     @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private AuthTokenCreator authTokenCreator;
 
     @Test
     @DisplayName("회원가입 - 성공")
@@ -64,5 +73,42 @@ class MemberServiceImplTest {
         CustomException customException = assertThrows(CustomException.class, () -> memberService.join(request));
 
         assertEquals(ErrorCode.DUPLICATE_ACCOUNT, customException.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("로그인 - 성공")
+    public void login() throws Exception {
+        //given
+        Member member = Member.testMemberEntity();
+
+        when(memberRepository.getByAccount("test1234")).thenReturn(member);
+        when(passwordEncoder.matches("test1234!@#$", "hashedPassword")).thenReturn(true);
+        when(authTokenCreator.createAuthToken(member.getId())).thenReturn("qwerqrwqrr.12323131.assdas");
+
+        //when
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "test1234!@#$");
+        MemberTokenResDto response = memberService.login(request);
+
+        //then
+        assertNotNull(response.accessToken());
+
+    }
+
+    @Test
+    @DisplayName("로그인 - 실패(잘못된 비밀번호)")
+    @Transactional
+    public void login_invalid_password() throws Exception {
+        //given
+        Member member = Member.testMemberEntity();
+
+        when(memberRepository.getByAccount("test1234")).thenReturn(member);
+        when(passwordEncoder.matches("wrongPassword", "hashedPassword")).thenReturn(false);
+
+        //when
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "wrongPassword");
+
+        //then
+        CustomException exception = assertThrows(CustomException.class, () -> memberService.login(request));
+        assertEquals(ErrorCode.INVALID_PASSWORD, exception.getErrorCode());
     }
 }
