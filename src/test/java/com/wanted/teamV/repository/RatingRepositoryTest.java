043895815811290acd4026372bf669ffad2616bd
Nayoon <nayoonk928@gmail.com>
2023-11-06 12:39:40 +0900

    fix #11 - RatingRepositoryTest, RestaurantRepositoryTest 수정 - JPAQueryFactory bean을 찾을 수 없는 에러 발생하여 수정

--- src/test/java/com/wanted/teamV/repository/RatingRepositoryTest.java
+++ src/test/java/com/wanted/teamV/repository/RatingRepositoryTest.java
@@ -1,20 +1,25 @@
 package com.wanted.teamV.repository;
 
+import com.wanted.teamV.config.QuerydslConfig;
 import com.wanted.teamV.entity.Member;
 import com.wanted.teamV.entity.Rating;
 import com.wanted.teamV.entity.Restaurant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Import({QuerydslConfig.class})
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class RatingRepositoryTest {
 
     @Autowired
