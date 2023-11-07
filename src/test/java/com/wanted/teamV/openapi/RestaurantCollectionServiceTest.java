59f3a68ad19728284fbbb29f4d717f892b212ddc
jjungyeun <wjyddd@naver.com>
2023-11-07 10:50:04 +0900

    test #21 - 데이터 파이프라인 통합테스트 작성
    - 스케줄링 테스트와 로직 통합테스트 분리
    - OpenAPI 부분은 mocking하여 간단한 테스트케이스 작성
    - 폐업한 식당을 제외하고 신규 식당을 저장하는 로직 테스트
    - 기존에 존재하던 식당의 정보를 업데이트하는 로직 테스트
    - 기존에 존재하던 식당이 폐업하면 삭제하는 로직 테스트

--- src/test/java/com/wanted/teamV/openapi/RestaurantCollectionServiceTest.java
+++ src/test/java/com/wanted/teamV/openapi/RestaurantCollectionServiceTest.java
@@ -1,50 +1,168 @@
 package com.wanted.teamV.openapi;
 
+import com.wanted.teamV.entity.Restaurant;
+import com.wanted.teamV.repository.RatingRepository;
 import com.wanted.teamV.repository.RestaurantRepository;
-import jakarta.persistence.EntityManager;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 @Transactional
-@SpringBootTest(properties = {"schedules.cron.restaurant-collection=0/10 * * * * *"})
+@SpringBootTest
 class RestaurantCollectionServiceTest {
 
+    @Mock
+    private OpenApiService apiService = mock(OpenApiService.class);
     @Autowired
-    EntityManager entityManager;
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private RatingRepository ratingRepository;
 
-    @Autowired
-    RestaurantRepository restaurantRepository;
+    private RestaurantCollectionService collectionService;
 
     @BeforeEach
     public void setUp() {
-        entityManager
-                .createNativeQuery("CREATE UNIQUE INDEX restaurant_name_addr_index on restaurant " +
-                        "(name, roadname_address)")
-                .executeUpdate();
+        collectionService = new RestaurantCollectionService(apiService, restaurantRepository, ratingRepository);
     }
 
     @Test
-    public void schedule_test() {
-        long beforeCnt = restaurantRepository.count();
-        Assertions.assertEquals(0, beforeCnt);
+    @DisplayName("폐업한 식당을 제외하고 새로운 식당 정보를 저장한다.")
+    public void closed_restaurant_is_not_saved() {
+        // given
+        Restaurant restaurant1 = Restaurant.builder()
+                .name("마니또김밥")
+                .sigun("화성시 ")
+                .licenseDe(LocalDateTime.of(2015, 2, 5, 0, 0, 0))
+                .bsnStateNm("영업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 영통로 59, 1층 112호 (반월동, 현대프라자)")
+                .lotAddress("경기도 화성시 반월동 869 현대프라자 112호")
+                .zipCode("18378")
+                .lon(127.0619018628)
+                .lat(37.2351254448)
+                .averageRating(0.0)
+                .build();
+        Restaurant restaurant2 = Restaurant.builder()
+                .name("김밥천국")
+                .sigun("화성시 ")
+                .licenseDe(LocalDateTime.of(1991, 6, 18, 0, 0, 0))
+                .bsnStateNm("폐업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 봉담읍 와우안길 48")
+                .lotAddress("경기도 화성시 봉담읍 와우리 75-3")
+                .zipCode("18321")
+                .lon(126.9764329175)
+                .lat(37.2160203201)
+                .averageRating(0.0)
+                .build();
 
-        Awaitility.await()
-                .atMost(3, TimeUnit.MINUTES)     // 최대 3분까지 확인
-                .pollDelay(Duration.ofSeconds(20))      // 처음 20초 기다림
-                .pollInterval(Duration.ofSeconds(5))    // 5초마다 조건 확인
-                .untilAsserted (() -> {
-                    long afterCnt = restaurantRepository.count();
-                    Assertions.assertTrue(afterCnt > 0);
-                });
+        when(apiService.rawToRestaurants(any())).thenReturn(List.of(restaurant1, restaurant2));
+
+        // when
+        collectionService.collectAllRestaurants();
+
+        // then
+        List<Restaurant> all = restaurantRepository.findAll();
+        Assertions.assertEquals(1, all.size());
     }
 
+    @Test
+    @DisplayName("기존에 저장된 식당에 대한 정보가 변경되면 DB에도 업데이트된다.")
+    public void update_restaurant_info() {
+        // given
+        restaurantRepository.save(Restaurant.builder()
+                .name("마니또김밥")
+                .sigun("화성시 ")
+                .licenseDe(LocalDateTime.of(2015, 2, 5, 0, 0, 0))
+                .bsnStateNm("영업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 영통로 59, 1층 112호 (반월동, 현대프라자)")
+                .lotAddress("경기도 화성시 반월동 869 현대프라자 112호")
+                .zipCode("18378")
+                .lon(127.0619018628)
+                .lat(37.2351254448)
+                .averageRating(0.0)
+                .build());
 
+        Restaurant updateRestaurant = Restaurant.builder()
+                .name("마니또김밥")
+                .sigun("화성시")
+                .licenseDe(LocalDateTime.of(2014, 2, 5, 0, 0, 0))
+                .bsnStateNm("영업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 영통로 59, 1층 112호 (반월동, 현대프라자)")
+                .lotAddress("경기도 화성시 반월동 869 현대프라자 112호")
+                .zipCode("18378")
+                .lon(127.0619018628)
+                .lat(37.2351254448)
+                .averageRating(0.0)
+                .totEmplyCnt(20)
+                .build();
+
+        when(apiService.rawToRestaurants(any())).thenReturn(List.of(updateRestaurant));
+
+        // when
+        collectionService.collectAllRestaurants();
+
+        // then
+        List<Restaurant> all = restaurantRepository.findAll();
+        Assertions.assertEquals(1, all.size());
+        Restaurant updated = all.get(0);
+        Assertions.assertEquals("화성시", updated.getSigun());
+        Assertions.assertEquals(LocalDateTime.of(2014, 2, 5, 0, 0, 0), updated.getLicenseDe());
+        Assertions.assertEquals(20, updated.getTotEmplyCnt());
+    }
+
+    @Test
+    @DisplayName("기존에 저장된 식당이 폐업하면 DB에서도 삭제된다.")
+    public void delete_closed_restaurant() {
+        // given
+        restaurantRepository.save(Restaurant.builder()
+                .name("마니또김밥")
+                .sigun("화성시 ")
+                .licenseDe(LocalDateTime.of(2015, 2, 5, 0, 0, 0))
+                .bsnStateNm("영업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 영통로 59, 1층 112호 (반월동, 현대프라자)")
+                .lotAddress("경기도 화성시 반월동 869 현대프라자 112호")
+                .zipCode("18378")
+                .lon(127.0619018628)
+                .lat(37.2351254448)
+                .averageRating(0.0)
+                .build());
+
+        Restaurant updateRestaurant = Restaurant.builder()
+                .name("마니또김밥")
+                .sigun("화성시")
+                .licenseDe(LocalDateTime.of(2014, 2, 5, 0, 0, 0))
+                .bsnStateNm("폐업")
+                .type("김밥(도시락)")
+                .roadnameAddress("경기도 화성시 영통로 59, 1층 112호 (반월동, 현대프라자)")
+                .lotAddress("경기도 화성시 반월동 869 현대프라자 112호")
+                .zipCode("18378")
+                .lon(127.0619018628)
+                .lat(37.2351254448)
+                .averageRating(0.0)
+                .totEmplyCnt(20)
+                .build();
+
+        when(apiService.rawToRestaurants(any())).thenReturn(List.of(updateRestaurant));
+
+        // when
+        collectionService.collectAllRestaurants();
+
+        // then
+        List<Restaurant> all = restaurantRepository.findAll();
+        Assertions.assertEquals(0, all.size());
+    }
 }
